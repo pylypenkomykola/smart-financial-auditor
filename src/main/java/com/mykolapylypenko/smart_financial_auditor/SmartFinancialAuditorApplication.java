8792045f8707efe2d26412400afe1ab73337094e
mykola <mayo@gft.com>
2026-05-07 22:53:14 +0200

    chore: initial project scaffold from start.spring.io
    Spring Boot 4.0.6 + Java 21 base project

---
+++ src/main/java/com/mykolapylypenko/smart_financial_auditor/SmartFinancialAuditorApplication.java
@@ -0,0 +1,13 @@
+package com.mykolapylypenko.smart_financial_auditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinancialAuditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFinancialAuditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mykolapylypenko/smart_financial_auditor/SmartFinancialAuditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykolapylypenko.smart_financial_auditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinancialAuditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
